e6524fca237bf13de3557bd1db7d6e3d5f6631fc
Livingston Akoma <66785918+SquidwardSama@users.noreply.github.com>
2021-09-25 19:31:19 -0400

    Update MavenMovies_Mid_Course_Project.sql

--- Maven Analytics/MavenMovies_Mid_Course_Project.sql
+++ Maven Analytics/MavenMovies_Mid_Course_Project.sql
@@ -5,58 +5,59 @@ use mavenmovies;
 email addresses, and the store identification number where they work. 
 */ 
 
-
-
-
-
-
-
+SELECT 
+    first_name, last_name, email, store_id
+FROM
+    staff;
 
 /*
 2.	We will need separate counts of inventory items held at each of your two stores. 
 */ 
 
-
-
-
-
-
+SELECT 
+    store_id, COUNT(inventory_id) AS 'Inventory Count'
+FROM
+    inventory
+GROUP BY store_id;
 
 /*
 3.	We will need a count of active customers for each of your stores. Separately, please. 
 */
 
-
-
-
-
-
-
+SELECT 
+    store_id, COUNT(customer_id) AS 'Active Customers'
+FROM
+    customer
+WHERE
+    active = 1
+GROUP BY store_id;
 
 /*
 4.	In order to assess the liability of a data breach, we will need you to provide a count 
 of all customer email addresses stored in the database. 
 */
 
-
-
-
-
-
+SELECT 
+   COUNT(email) as 'Emails Stored'
+FROM
+    customer;
+    
 /*
 5.	We are interested in how diverse your film offering is as a means of understanding how likely 
 you are to keep customers engaged in the future. Please provide a count of unique film titles 
 you have in inventory at each store and then provide a count of the unique categories of films you provide. 
 */
 
+SELECT 
+    store_id, COUNT(DISTINCT film_id) AS 'Unique Titles'
+FROM
+    inventory
+GROUP BY store_id;
 
-
-
-
-
-
-
-
+SELECT 
+    COUNT(DISTINCT name) AS 'Unique Categories'
+FROM
+    category;
 
 /*
 6.	We would like to understand the replacement cost of your films. 
@@ -64,11 +65,12 @@ Please provide the replacement cost for the film that is least expensive to repl
 the most expensive to replace, and the average of all films you carry. ``	
 */
 
-
-
-
-
-
+SELECT 
+    MIN(replacement_cost) AS 'Least Expensive',
+    MAX(replacement_cost) AS 'Most Expensive',
+    AVG(replacement_cost) AS 'Average Replacement Cost'
+FROM
+    film;
 
 /*
 7.	We are interested in having you put payment monitoring systems and maximum payment 
@@ -76,9 +78,11 @@ processing restrictions in place in order to minimize the future risk of fraud b
 Please provide the average payment you process, as well as the maximum payment you have processed.
 */
 
-
-
-
+SELECT 
+    AVG(amount) AS 'Avg Payment Processed',
+    MAX(amount) AS 'Max Payment Processed'
+FROM
+    payment;
 
 /*
 8.	We would like to better understand what your customer base looks like. 
@@ -86,5 +90,11 @@ Please provide a list of all customer identification values, with a count of ren
 they have made all-time, with your highest volume customers at the top of the list.
 */
 
+SELECT 
+    customer_id, COUNT(rental_id) AS 'Rental Count'
+FROM
+    rental
+GROUP BY customer_id
+ORDER BY COUNT(rental_id) DESC;
 
 
